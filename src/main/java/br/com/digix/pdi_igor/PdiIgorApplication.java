0d42d49338887ad8a8978f82d282498ae01eacde
Igor Francozo <igorfrancozo@digix.com.br>
2024-11-24 15:25:36 -0400

    Inicio do projeto

---
+++ src/main/java/br/com/digix/pdi_igor/PdiIgorApplication.java
@@ -0,0 +1,13 @@
+package br.com.digix.pdi_igor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdiIgorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdiIgorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digix/pdi_igor/PdiIgorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digix.pdi_igor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdiIgorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
