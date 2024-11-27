d92e26ab2ec52ab99e787d69a529a4225ffe5ed0
Igor Francozo <igorfrancozo@digix.com.br>
2024-11-27 14:23:08 -0400

    adição de builder

---
+++ src/test/java/br/com/digix/pdi_igor/dominio/MacronutrientesTest.java
@@ -0,0 +1,14 @@
+package br.com.digix.pdi_igor.dominio;
+
+import br.com.digix.pdi_igor.dominio.builder.MacronutrientesBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MacronutrientesTest {
+
+    @Test
+    void deve_criar_um_macronutriente() {
+
+    }
+}
